10e9729055f9bc03905f22fc720a3fcca2d085ee
Darshana <thaveeshadarshana@gmail.com>
2025-08-11 10:50:24 +0800

    sddsdsd

--- com.demo/src/test/java/com/demo/DemoMain.java
+++ com.demo/src/test/java/com/demo/DemoMain.java
@@ -2,13 +2,14 @@ package com.demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoMain {
 
-	public static void main(String[] args) {
-	
+	@Test
+	public void mainTest() {
 		// Setup ChromeDriver automatically using WebDriverManager
         WebDriverManager.chromedriver().setup();
 
